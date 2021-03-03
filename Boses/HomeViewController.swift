ec08545352b952f658a2be3ffa6a336e121e7324
Ritz Ballares <ritzballares@gmail.com>
2021-03-02 22:42:35 -0500

    Initial commit

---
+++ Boses/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Boses
+//
+//  Created by Ritz Ballares on 3/2/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    @IBAction func learnButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToLearn", sender: nil)
+    }
+    
+}
+
---
+++ Boses/LearnViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LearnViewController.swift
+//  Boses
+//
+//  Created by Ritz Ballares on 3/2/21.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationController?.isNavigationBarHidden = false
+        self.title = "ASL Alphabet"
+    }
+    
+}
