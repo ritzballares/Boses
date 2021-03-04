ec9f7d87afcbe45f27f48ef63402e4d61c4bc281
Ritz Ballares <ritzballares@gmail.com>
2021-03-04 08:33:14 -0500

    Moved files to Model and Controller folders

--- Boses/Model/Letter.swift
+++ Boses/Model/Letter.swift
@@ -5,7 +5,7 @@
 //  Created by Ritz Ballares on 3/4/21.
 //
 
-import Foundation
+import UIKit
 
 struct Letter {
     let letterImages: [UIImage] = [#imageLiteral(resourceName: "a.jpg"), #imageLiteral(resourceName: "b.jpg"), #imageLiteral(resourceName: "c.jpg"), #imageLiteral(resourceName: "d.jpg"), #imageLiteral(resourceName: "e.jpg"), #imageLiteral(resourceName: "f.jpg"), #imageLiteral(resourceName: "g.jpg"), #imageLiteral(resourceName: "h.jpg"), #imageLiteral(resourceName: "i.jpg"), #imageLiteral(resourceName: "j.jpg"), #imageLiteral(resourceName: "k.jpg"), #imageLiteral(resourceName: "l.jpg"), #imageLiteral(resourceName: "m.jpg"), #imageLiteral(resourceName: "n.jpg"), #imageLiteral(resourceName: "o.jpg"), #imageLiteral(resourceName: "p.jpg"), #imageLiteral(resourceName: "q.jpg"), #imageLiteral(resourceName: "r.jpg"), #imageLiteral(resourceName: "s.jpg"), #imageLiteral(resourceName: "t.jpg"), #imageLiteral(resourceName: "u.jpg"), #imageLiteral(resourceName: "v.jpg"), #imageLiteral(resourceName: "w.jpg"), #imageLiteral(resourceName: "x.jpg"), #imageLiteral(resourceName: "y.jpg"), #imageLiteral(resourceName: "z.jpg")]
