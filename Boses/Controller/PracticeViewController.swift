cab29d5b7944b917a26d217719b3416d606ce9c6
Ritz Ballares <ritzballares@gmail.com>
2021-03-12 09:51:47 -0500

    Updated PracticeViewController.swift -- successfully passed data to ML model

--- Boses/Controller/PracticeViewController.swift
+++ Boses/Controller/PracticeViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import AVKit
+import Vision
 
-class PracticeViewController: UIViewController {
+class PracticeViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var letter: Letter?
     
     override func viewDidLoad() {
@@ -18,6 +19,7 @@ class PracticeViewController: UIViewController {
     
     func setUpAV() {
         let session = AVCaptureSession()
+        session.sessionPreset = .photo
         
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return
         }
@@ -33,6 +35,40 @@ class PracticeViewController: UIViewController {
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
         
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        session.addOutput(dataOutput)
+        
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let model = try? VNCoreMLModel(for: ASLDetector().model) else {
+            print("Error occured during initialization of CoreML model")
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (req, err) in
+ 
+            if let results = req.results {
+                for observation in results {
+                    guard let obv = observation as? VNRecognizedObjectObservation else {return}
+                    print(obv.labels[0])
+                }
+            }
+            
+
+        }
+        
+        do {
+            try VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        } catch {
+            print("Something happened with the image request handler")
+            return
+        }
         
     }
 }
