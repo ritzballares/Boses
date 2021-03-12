0e8231ad3b907ec6fe88c3d86b990f808edccd1c
Ritz Ballares <ritzballares@gmail.com>
2021-03-11 20:25:38 -0500

    Added PracticeViewController

---
+++ Boses/Controller/PracticeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PracticeViewController.swift
+//  Boses
+//
+//  Created by Ritz Ballares on 3/11/21.
+//
+
+import UIKit
+
+class PracticeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setUpAV() {
+        
+    }
+}
