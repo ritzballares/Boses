b089f15c31e876ba7ab836caf41cc331032dd0f4
Ritz Ballares <ritzballares@gmail.com>
2021-04-11 16:01:55 -0400

    Updated practice view controller

--- Boses/Controller/PracticeViewController.swift
+++ Boses/Controller/PracticeViewController.swift
@@ -11,6 +11,7 @@ import Vision
 
 class PracticeViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var letter: Letter?
+    @IBOutlet weak var previewView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,57 +19,99 @@ class PracticeViewController: UIViewController, AVCaptureVideoDataOutputSampleBu
     }
     
     func setUpAV() {
+        // configure the camera to use for capture
         let session = AVCaptureSession()
-        session.sessionPreset = .photo
         
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return
+        // set device and session resolution
+        let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .front).devices.first
+        
+        guard let deviceInput = try? AVCaptureDeviceInput(device: videoDevice!) else {
+            print("Could not create video device input")
+            return
         }
         
-        guard let deviceInput = try? AVCaptureDeviceInput(device: captureDevice) else {
+        session.beginConfiguration()
+        session.sessionPreset = .vga640x480
+        
+        // add video input to session
+        guard session.canAddInput(deviceInput) else {
+            print("Could not add video device input to the session")
+            session.commitConfiguration()
             return
         }
         
         session.addInput(deviceInput)
-        session.startRunning()
         
-        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
+        // add video output to session
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
         
-        let dataOutput = AVCaptureVideoDataOutput()
-        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-        session.addOutput(dataOutput)
-        
-        
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        
-        guard let model = try? VNCoreMLModel(for: ASLDetector().model) else {
-            print("Error occured during initialization of CoreML model")
+        if session.canAddOutput(videoDataOutput) {
+            session.addOutput(videoDataOutput)
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            print("Could not add video data output to the session")
+            session.commitConfiguration()
             return
         }
         
-        let request = VNCoreMLRequest(model: model) { (req, err) in
- 
-            if let results = req.results {
-                for observation in results {
-                    guard let obv = observation as? VNRecognizedObjectObservation else {return}
-                    print(obv.labels[0])
-                }
-            }
-            
-
-        }
+        // process every frame, but don't hold on to more than one Vision request at a time
+        let captureConnection = videoDataOutput.connection(with: .video)
+        captureConnection?.isEnabled = true
+        var bufferSize: CGSize = .zero
         
         do {
-            try VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+            try videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
+            videoDevice!.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+        
+        session.commitConfiguration()
+        session.startRunning()
+        
+        // set up preview layer
+        var previewLayer: AVCaptureVideoPreviewLayer! = nil
+        var rootLayer: CALayer! = nil
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        rootLayer = previewView.layer
+        previewLayer.frame = rootLayer.bounds
+        rootLayer.addSublayer(previewLayer)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let model = try? ASLDetector(configuration: MLModelConfiguration()),
+              let visionModel = try? VNCoreMLModel(for: model.model) else {
+            print("Could not load model")
+            return
+        }
+        
+        let objectRecognition = VNCoreMLRequest(model: visionModel) { (req, err) in
+            DispatchQueue.main.async(execute: {
+                if let results = req.results {
+                    for observation in results where observation is VNRecognizedObjectObservation {
+                        guard let objectObservation = observation as? VNRecognizedObjectObservation else { continue }
+                        let topLabelObservation = objectObservation.labels[0]
+                        print(topLabelObservation)
+                    }
+                }
+            })
+        }
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+
+        do {
+            try VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([objectRecognition])
         } catch {
             print("Something happened with the image request handler")
             return
         }
-        
     }
 }
